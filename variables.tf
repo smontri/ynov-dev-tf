17be919af6ecb0efef002f7194e3f1a6b9c3ff9e
Stéphane MONTRI <stephane@mac.home>
2025-03-21 10:27:56 +0100

    add TF files

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "prefix" {
+  # A personnaliser avec votre nom
+  default = "ci-cd"
+}
+
+variable "environment" {
+  default = "demo"
+}
+
+variable "project" {
+  default = "ci-cd"
+}
+
+variable "ssh_key" {
+  default = ""
+}
